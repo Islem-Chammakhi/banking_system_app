83e9abbc642378616894353dcb2c9d6f2f94f046
islem <islemchammakhi@gmail.com>
2025-12-02 18:25:18 +0100

    add models

---
+++ backend/src/main/java/com/securite/securite/models/AccountStatus.java
@@ -0,0 +1,6 @@
+package com.securite.securite.models;
+
+
+public enum AccountStatus {
+    ACTIVE, SUSPENDED
+}
---
+++ backend/src/main/java/com/securite/securite/models/TransactionStatus.java
@@ -0,0 +1,6 @@
+package com.securite.securite.models;
+
+
+public enum TransactionStatus {
+    PENDING, COMPLETED, FAILED
+}
